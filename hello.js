627c73b6f139857dbc1e8d7f69756de960565697
Haiden Taylor <haiden@asciant.com>
2019-12-01 12:35:16 +1000

    File from tutorial, updated hello to test.

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from iMac\n');
+  res.end('Hello from Updated Tutorial on iMac\n');
 }).listen(8080, '10.130.97.47');
 console.log('Server running at http://10.130.97.47:8080/');
